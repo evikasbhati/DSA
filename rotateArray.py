3914c99642b77dbbe9092f62ac50026ea2ffc30a
VIKAS BHATI <evikasbhati@gmail.com>
2023-05-04 14:35:19 +0530

    rotate array prog added

--- rotateArray.py
+++ rotateArray.py
@@ -1,11 +1,15 @@
 # Rotation of aaray means shiftig array elements postion to left or right
 # by specific place.
 ar=[3, 4, 7, 8, 9, 11, 17, 19, 28];
+def rotateArray(arr,postion):
+    n=len(ar)
+    temp=[];
+    for i in range(len(arr)):
+        temp.insert((i+postion)%n, ar[i])
+        print((i+postion)%n)
 
-def rotateArray(arr,postion,dir="right"):
-    for i in range(len(arr)-1,postion-1,-1):
-        (arr[i],arr[i-postion])=(arr[i-postion],arr[i])
+    return temp
+    
 
 
-rotateArray(ar,3)
-print(ar)
+print(rotateArray(ar,3))
